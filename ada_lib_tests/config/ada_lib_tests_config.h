c080bfc67e4f8f2c2e8c17058c74bbb66539ba74
wbullaughey <wlb122@verizon.net>
2025-02-09 19:57:50 -0500

    merge to laptop 2/9/25

--- ada_lib_tests/config/ada_lib_tests_config.h
+++ ada_lib_tests/config/ada_lib_tests_config.h
@@ -7,7 +7,7 @@
 
 #define ALIRE_HOST_OS "macos"
 
-#define ALIRE_HOST_ARCH "x86_64"
+#define ALIRE_HOST_ARCH "aarch64"
 
 #define ALIRE_HOST_DISTRO "homebrew"
 
@@ -15,6 +15,6 @@
 #define BUILD_PROFILE_VALIDATION 2
 #define BUILD_PROFILE_DEVELOPMENT 3
 
-#define BUILD_PROFILE  3
+#define BUILD_PROFILE  1
 
 #endif
