24be49f393608469097612944c14b4dec4960fc8
wbullaughey <wlb122@verizon.net>
2025-02-09 20:23:45 -0500

    merge to laptop 2/9/25

--- ada_lib_tests/build.sh
+++ ada_lib_tests/build.sh
@@ -1,12 +1,3 @@
-#!/bin/zsh
-echo build ada_lib tests parameters $*
-#pwd
-if [ -z "$2" ]; then
-  echo "initial build"
-else
-  echo "remote build"
-  cd $2
-fi
-
-#ls -l ../../*.sh
-../../remote_build.sh alr_environment ada_lib/ada_lib_tests test_ada_lib
+source ~/.zshrc
+which alr
+alr -v build -- -j10 -s -k -gnatE
