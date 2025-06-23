c0e3f6348c30a3b36b7cf866ca0b31225f68c44c
wbullaughey <wlb122@verizon.net>
2025-06-23 08:20:08 -0400

    merge to laptop 6/23/25

--- ada_lib_tests/build.sh
+++ ada_lib_tests/build.sh
@@ -1,9 +1,11 @@
 source ~/.zshrc
 export WHICH=$1
-export DIRECTORY=`pwd`
-echo "options $0 WHICH $WHICH \
-   BUILD_PROFILE $ADA_APPLICATION_PROFILE \
-   ADA_OS_INCLUDE $ADA_OS_INCLUDE \
-   DIRECTORY $DIRECTORY"
 
-../../global_build.sh $WHICH $ADA_APPLICATION_PROFILE program
+# WHICH values
+#   all     - build everything (help_tests, driver unit tests, applications)
+#   execute    - build application or library for subdirectory level
+#   help_test  - builds help_test at level
+
+echo build WHICH $WHICH
+
+../../global_build.sh $WHICH program
